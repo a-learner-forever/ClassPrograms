d19334f2e4da26cc665daafdd14096f4aa42820b
Asmita <64234753+a-learner-forever@users.noreply.github.com>
2026-02-13 20:20:56 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -8,3 +8,14 @@ class Student:
 
 ob1 = Student('s001','Arun')
 ob1.display()
+
+class Vehicle:
+    def __init__(self,vid,vname):
+        self.vid = vid
+        self.vname = vname
+
+    def display(self):
+        print('The id and name of the vehicle are', self.vid, self.vname)
+
+ob1 = Vehicle('v001','Audi')
+ob1.display()
